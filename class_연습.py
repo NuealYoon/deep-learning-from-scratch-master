0b3cadc16e7deba0cfea11d625b40da884e279ed
NuealYoon <muleng.suc@gmail.com>
2021-01-02 18:25:26 +0900

    클래스 연습 코드

---
+++ class_연습.py
@@ -0,0 +1,30 @@
+# Class for animals
+class Animal:
+    category = "pets"
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def printname(self):
+        print("Name of this animal is: ", self.name)
+
+    def printage(self):
+        print("Age of this animal is: ", self.age)
+
+    def move(self):
+        print("Running")
+
+
+# Instantiate Animal objects
+dog = Animal("Titu", 5)
+cat = Animal("Sarah", 6)
+
+# Call Instance Methods
+dog.printname()
+dog.printage()
+dog.move()
+
+cat.printname()
+cat.printage()
+cat.move()
